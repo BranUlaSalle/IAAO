c0fc8a61c7e60040d77af61a45180a6f46871429
BRAN <bpazp@ulasalle.edu.pe>
2026-04-20 10:21:08 -0500

    agrego documento y agrego contador al selection sort

--- Actividad_4/SelectionSort.java
+++ Actividad_4/SelectionSort.java
@@ -5,15 +5,18 @@ import java.util.Arrays;
 public class SelectionSort {
     public static void main(String[] args) {
         int[] arr = {5, 4, 3, 2, 1};
+        int contador=0;
         for (int i = 0; i < arr.length - 1; i++) {
             int min = i;
             for (int j = i + 1; j < arr.length; j++) {
                 if (arr[j] < arr[min]) min = j;
+                contador++;
             }
             int tmp = arr[min];
             arr[min] = arr[i];
             arr[i] = tmp;
         }
         System.out.println(Arrays.toString(arr));
+        System.out.println("contador: "+contador);
     }
 }

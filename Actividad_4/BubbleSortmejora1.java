381c110b4250b52f35410d1dffbf9171b5878c71
BRAN <bpazp@ulasalle.edu.pe>
2026-04-19 21:17:35 -0500

    primera modificacion en bbuble

--- Actividad_4/BubbleSortmejora1.java
+++ Actividad_4/BubbleSortmejora1.java
@@ -4,13 +4,20 @@ import java.util.Arrays;
 public class BubbleSortmejora1 {
     public static void main (String[] args){
         int[] arr = {5,4,3,2,1};
+        int swapped=0;
+
         for (int i = 0; i< arr.length - 1; i++){
+            swapped=0; 
             for (int j = 0; j < arr.length - i - 1; j++ ){
                 if (arr[j] > arr[ j+1]){
                     int tmp= arr[j];
                     arr[j] = arr[j+1];
-                    arr[j+1] = tmp; 
+                    arr[j+1] = tmp;
+                    swapped=1;
                 }
+            }
+            if(swapped !=0){
+                break; 
             }
         }
         System.out.println(Arrays.toString(arr));

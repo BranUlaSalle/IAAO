5a24b425c43ee6e942a1a4cf2e66cadc533a7345
BRAN <bpazp@ulasalle.edu.pe>
2026-04-20 10:21:56 -0500

    agrego comentario al contador

--- Actividad_4/SelectionSort.java
+++ Actividad_4/SelectionSort.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class SelectionSort {
     public static void main(String[] args) {
         int[] arr = {5, 4, 3, 2, 1};
+        //contador 
         int contador=0;
         for (int i = 0; i < arr.length - 1; i++) {
             int min = i;
